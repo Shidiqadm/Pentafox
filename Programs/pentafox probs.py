90a4e5f22ef4f32f1fea18f0694d1087bde02d59
Shidiq Ahmed <35922195+Shidiqadm@users.noreply.github.com>
2021-04-05 08:03:14 +0530

    Update pentafox probs.py

--- Programs/pentafox probs.py
+++ Programs/pentafox probs.py
@@ -157,7 +157,6 @@ upper = int(input("ENter the maximum value: "))
 print("Prime numbers between", lower, "and", upper, "are:")
 
 for num in range(lower, upper + 1):
-   # all prime numbers are greater than 1
    if num > 1:
        for i in range(2, num):
            if (num % i) == 0:
